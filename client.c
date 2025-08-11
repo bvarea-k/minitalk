919395183c4c7acd54682f1bb8fe12abccf8419b
bvarea-k <begovk@gmail.com>
2025-08-11 17:08:55 +0200

    Update client.c

--- client.c
+++ client.c
@@ -4,10 +4,12 @@
 
 int main(int ac, char **av)
 {
+    int pid;
     if (ac != 3)
     {
         ft_printf("ERROR, invalid number or params");
         exit(EXIT_FAILURE);
     }
+    pid = ft_atoi(av[1]); // o no???????????????????
     return (EXIT_SUCCESS);
 }
