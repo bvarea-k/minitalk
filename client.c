00f8fd3136b8b81b6fe382d46f33793b8983d3f3
bvarea-k <begovk@gmail.com>
2025-08-20 16:33:44 +0200

    Update client.c

--- client.c
+++ client.c
@@ -12,19 +12,28 @@
 
 #include "minitalk.h"
 
+//El cliente envía el mensaje al servidor con la señales SIGUSR1(0) y SIGUSR2(1).
 //Necesito variable global para que cambie en cualquier momento del programa.
 //Es volatile para indicar que puede cambiar de forma inesperada, para que el programa la lea cada vez que la use.
 //Es sig_atomic_t para garantizar que su lectura y escritura sean atómicas (no se corten por la mitad).
 //El cliente usa sa_handler y SA_RESTART, porque solo necesita saber si le llegó una señal para activar un flag.
 //El servidor usa sa_sigaction y SA_SIGINFO porque necesita saber quién le envió la señal.
 
-volatile sig_atomic_t	g_ack_received = 0;
+volatile sig_atomic_t	g_ack_received = 0; //El cliente la usa para verificar que el servidor ha recibido bien cada señal
 
 static void	signal_handler(int signum)
 {
 	(void)signum;
-	g_ack_received = 1;
+	g_ack_received = 1; // El servidor ha enviado una confirmación (SIGUSR1)
 }
+
+
+//El cliente envía el mensaje bit a bit.
+//Si es 1, manda SIGUSR2 y, si es 0, manda SIGUSR1.
+//Después de cada señal, el cliente espera a recibir la confirmación (SIGUSR1)
+//de que el servidor ha procesado la señal.**
+//La espera está limitada a 10,000 ciclos de espera con usleep(100)
+//para evitar que el cliente quede bloqueado indefinidamente.
 
 static void	send_char(int pid, char c)
 {
@@ -36,12 +45,12 @@ static void	send_char(int pid, char c)
 	{
 		g_ack_received = 0;
 		if ((c >> bit) & 1)
-			kill(pid, SIGUSR2);
+			kill(pid, SIGUSR2); //Envía SIGUSR2 si el bit es 1
 		else
-			kill(pid, SIGUSR1);
+			kill(pid, SIGUSR1); // Envía SIGUSR1 si el bit es 0
 		wait = 0;
 		while (!g_ack_received && wait++ < 10000)
-			usleep(100);
+			usleep(100); // Espera la confirmación del servidor**
 		bit--;
 	}
 }
@@ -75,12 +84,28 @@ int	main(int argc, char **argv)
 	int					i;
 
 	i = 0;
-	validate_argc(argc);
-	pid = validate_pid(argv[1]);
-	msg = argv[2];
-	setup_sigaction(&sa);
+	validate_argc(argc); //Valido los argumentos.
+	pid = validate_pid(argv[1]); //Valido el PID
+	msg = argv[2]; //Obtengo el mensaje del cliente.
+	setup_sigaction(&sa); //Configuro el handler (manejador)
 	while (msg[i])
-		send_char(pid, msg[i++]);
-	send_char(pid, '\0');
+		send_char(pid, msg[i++]);//Envío cada caracter del mensaje,
+	send_char(pid, '\0');//Envío el nulo al final del mensaje.
 	return (0);
 }
+
+/*								Flujo básico:
+		El cliente:
+  1. Envía un bit.
+  2. Después de mandar cada bit, entra en un bucle de espera hasta que el servidor manda SIGUSR1.
+  Este bucle está controlado por la variable global. Si no recibe la confirmación!!!! el contador wait
+  se incrementa hasta 10000 intentos (para darle tieeeeeeeempo).
+		El servidor:
+  1. Espera recibir los bits del cliente. A medida que los va recibiendo, cuenta cuántos bits ha recibido (bit_nbr).
+  2. Procesa el carácter completo y lo imprime.
+  3. Envía SIGUSR1 al cliente.
+- Espera las señales SIGUSR1 y SIGUSR2.
+- Recibe los bits.
+- Cuando hay 8 bits (el caracter está completo), imprime.
+- Envía una señal de confirmación (SIGUSR1, o sea 0 en mi caso) al cliente con cada caracter.
+*/
